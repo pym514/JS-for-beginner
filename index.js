938fb2fc21d369c18fa615179eab9320541b6c14
pym514 <yongman514@gmail.com>
2020-08-24 14:21:29 +0900

    JS: comment

--- index.js
+++ index.js
@@ -1,2 +1,18 @@
-console.log("Study JS, this is alert");
-alert("Study JS");
+/* String
+const what = "yong";
+*/
+
+/*
+Boolean
+const wat = true
+it is not a text
+*/
+
+/*
+Number
+const wat = 777;
+*/
+
+/*Float
+const wat = 55.1;
+*/
